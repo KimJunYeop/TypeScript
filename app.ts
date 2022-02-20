7cf66923a96f9a727eb66f1fd268c0bb0e5331a3
kimjunyeop <wnsduq8282@gmail.com>
2022-02-20 15:45:09 +0900

    tuple

--- app.ts
+++ app.ts
@@ -6,11 +6,21 @@
 //     age: 30
 // }
 
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
     name: 'Maximlian',
     age: 30,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 };
+
+person.role.push('admin')
+// person.role[1] = 10;
+
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
