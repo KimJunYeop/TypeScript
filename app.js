a43d437089f6ad8ba8c1871683a05daae032b413
kimjunyeop <wnsduq8282@gmail.com>
2022-02-20 15:40:24 +0900

    array for ts

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+//     name: 'Maximlian',
+//     age: 30
+// }
 var person = {
     name: 'Maximlian',
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
 };
-console.log(person.nickname);
+var favoriteActivities;
+favoriteActivities = ['Sports'];
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- app.ts
+++ app.ts
@@ -8,7 +8,14 @@
 
 const person = {
     name: 'Maximlian',
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
+};
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase())
 }
 
-console.log(person.name);
